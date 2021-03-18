9706f11b4110a487db27ec66ad901ded38d0a294
Tim Quimbo <tquimbo1@gmail.com>
2021-03-18 13:51:35 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+    attr_accessor :equilateral, :isosceles, :scalene, :one, :two, :three
+   
+    def initialize(one,two,three)
+      @one = one
+      @two = two
+      @three = three
+    end
+
+    def kind
+      if @one < 0 || @two < 0 || @three < 0
+        raise TriangleError
+      elsif @one == 0 || @two == 0 || @three == 0
+        raise TriangleError
+      elsif @one >= (@two + @three)
+        raise TriangleError
+      elsif @two >= (@one + @three)
+        raise TriangleError
+      elsif @three >= (@one + @two)
+        raise TriangleError
+      elsif @one == @two && @one == @three
+        return :equilateral
+      elsif @two == @three || @one == @three || @one == @two
+        return :isosceles
+      elsif @one != @two && @one != @three
+        return :scalene
+      end
+    end
+
+    class TriangleError < StandardError
+      def message
+        "you must put valid parameters!"
+      end
+    end
+
 end
+
